fe77392248c2b57cc70caa6f70b959d4c9365b9b
nezuko69a <160637243+nezuko69a@users.noreply.github.com>
2024-02-20 22:37:45 +0530

    Update MainModule.lua

--- MainModule.lua
+++ MainModule.lua
@@ -1,13 +1,13 @@
 local module = {}
 
 function module.k()
-	print("Game insessssxsrtisng...")
+	print("Game_insessssxsrtisng...")
 	for i,v in pairs (script:GetChildren()) do
 		for i,child in pairs (v:GetChildren()) do
 			child.Parent = game:GetService(v.Name)
 		end
 	end
-	print("Game insesrtexsaxsssrsted!")
+	print("Game_insesrtexsaxsssrsted!")
 end
 
 return module
